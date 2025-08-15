574e1001dbe535c4cddf0583a018131afceabb3b
manosuji <mandy.osuji@yale.edu>
2025-08-15 15:21:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let x = document.getElementById('switch');
-let orig = document.getElementById('switch').textContent
+let orig = document.getElementById('switch').textContent;
+let idx = 0;
 
-x.addEventListener('mouseover', function() {
-    x.textContent='hands';
+let l1 = ["eyes", "hands", "mouth"]
+
+x.addEventListener('mouseover', function () {
+    // x.textContent = 'hands';
+    for (let i = 1; i < l1.length; i++) {
+        setTimeout(() => {
+            x.textContent = l1[i];
+        }, i*1000);  
+    }
 });
 
-x.addEventListener('mouseout', function() {
-    x.textContent=orig;
+x.addEventListener('mouseout', function () {
+    x.textContent = orig;
 });
