c1dc854ca5ec991ce5b0568476e213d079ea9a69
manosuji <mandy.osuji@yale.edu>
2025-08-15 15:54:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-let x = document.getElementById('switch');
-let orig = document.getElementById('switch').textContent;
-let idx = 0;
+let x = document.getElementById('switch'); // initial text
+let orig = document.getElementById('switch').textContent; // original text
 
-let l1 = ["eyes", "hands", "mouth"]
+let idx = 0; // current index of item displayed in list
+let l1 = ["sky", "hands", "mouth"] // list of words to display
 
+// when mouse over word, cycle through list of words
 x.addEventListener('mouseover', function () {
-    // x.textContent = 'hands';
-    for (let i = 1; i < l1.length; i++) {
-        setTimeout(() => {
-            x.textContent = l1[i];
-        }, i*1000);  
-    }
+        x.textContent=nextItem(l1);
 });
 
-x.addEventListener('mouseout', function () {
-    x.textContent = orig;
-});
+// returns the next item in a list, returning to beginning of list if needed
+function nextItem(list) {
+    if (idx >= list.length) {
+        idx = 0;
+    }
+    item = list[idx];
+    idx++;
+    return item;
+}
