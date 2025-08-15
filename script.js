27955ceb5e7fd9b0f56fb561e40aeee2ee27969f
manosuji <mandy.osuji@yale.edu>
2025-08-15 15:56:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 let x = document.getElementById('switch'); // initial text
-let orig = document.getElementById('switch').textContent; // original text
 
 let idx = 0; // current index of item displayed in list
 let l1 = ["sky", "hands", "mouth"] // list of words to display
 
-// when mouse over word, cycle through list of words
+// when mouse over word, go to next word in list
 x.addEventListener('mouseover', function () {
         x.textContent=nextItem(l1);
 });
