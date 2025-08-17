b16613e20cbd3d629a2360f88a3946022225ed0e
manosuji <mandy.osuji@yale.edu>
2025-08-16 19:20:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-let x = document.getElementById('switch'); // initial text
-
-let idx = 0; // current index of item displayed in list
-let line1 = ["sky", "hands", "mouth"] // list of words to display
-
-// when mouse over word, go to next word in list
-x.addEventListener('mouseover', function () {
-        x.textContent=nextItem(line1);
-});
-
 // returns the next item in a list, returning to beginning of list if needed
+let idx = 0; // current index of item displayed in list
 function nextItem(list) {
     if (idx >= list.length) {
         idx = 0;
@@ -17,3 +8,42 @@ function nextItem(list) {
     idx++;
     return item;
 }
+
+let line1 = ["sky", "hands", "mouth"]
+
+var changers = document.querySelectorAll(".changer");
+
+changers.forEach(item => {
+    item.addEventListener('mouseover', function () {
+        item.textContent=nextItem(item.dataset.alts.split('/'));
+    });
+    item.addEventListener('click', function () {
+        item.textContent=nextItem(item.dataset.alts.split('/'));
+    });  
+})
+
+
+// for (var i=0;i<changers.length;i++) {
+//     changers[i].addEventListener('mouseover', function () {
+//         changers[i].textContent=nextItem(line1);
+//     });
+//     changers[i].addEventListener('click', function () {
+//         changers[i].textContent=nextItem(line1);
+//     });
+// }
+
+//WORKING BELOW
+
+// let x = document.getElementById('eyes'); // initial text
+
+// let line1 = ["sky", "hands", "mouth"] // list of words to display
+
+// // when mouse over word, go to next word in list
+// x.addEventListener('mouseover', function () {
+//         x.textContent=nextItem(line1);
+// });
+
+// x.addEventListener('click', function () {
+//     x.textContent=nextItem(line1);
+// });
+
